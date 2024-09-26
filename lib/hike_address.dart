a34a67a3a679e5b824cc90ee692d98b69e25dd35
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-09-26 10:53:41 +0200

    implemented "counter++"

--- lib/hike_address.dart
+++ lib/hike_address.dart
@@ -1,22 +1,63 @@
 import 'package:design_to_flutter_ui/hike_name_and_address.dart';
 import 'package:flutter/material.dart';
 
-class HikeAddress extends StatelessWidget {
-  const HikeAddress({
-    super.key,
-  });
+class HikeAddress extends StatefulWidget {
+  const HikeAddress({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        HikeNameAndAddress(),
-        Spacer(),
-        Icon(Icons.star, color: Colors.red),
-        SizedBox(width: 4),
-        Text("41"),
-      ],
-    );
-  }
+  State<HikeAddress> createState() => HikeAdressState();
 }
+
+class HikeAdressState extends State<HikeAddress> {
+  int counter = 41;
+  IconData iconData = Icons.star;
+
+
+@override
+Widget build(BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const HikeNameAndAddress(),
+      const Spacer(),
+      IconButton(
+        onPressed: () {
+          print("Button gedrückt");  // zeigt nur in der Debug-Konsole den Text an.
+          setState(() {
+            counter++;
+          });
+        },
+        icon: Icon(iconData, color: Colors.red),
+      ),
+      const SizedBox(width: 4),
+       Text(counter.toString()),
+    ],
+  );
+}
+  
+}
+
+// class HikeAddress extends StatelessWidget {
+//   const HikeAddress({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         HikeNameAndAddress(),
+//         Spacer(),
+//         IconButton(
+//           onPressed: () {
+//             print("Button gedrückt");
+//           },
+//           icon: Icon(Icons.star, color: Colors.red),
+//         ),
+//         const SizedBox(width: 4),
+//         const Text("41"),
+//       ],
+//     );
+//   }
+// }
